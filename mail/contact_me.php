de409acc71b7af47f93f20a30a89183a3160d8cb
Mkassabov <35264484+Mkassabov@users.noreply.github.com>
2018-08-30 08:34:19 -0400

    Init Push

---
+++ mail/contact_me.php
@@ -0,0 +1,29 @@
+<?php
+// Check for empty fields
+if(empty($_POST['first'])     ||
+   empty($_POST['last'])      ||
+   empty($_POST['company'])   ||
+   empty($_POST['email'])     ||
+   empty($_POST['phone'])     ||
+   empty($_POST['url'])       ||
+   empty($_POST['message'])   ||
+   !filter_var($_POST['email'],FILTER_VALIDATE_EMAIL))
+   {
+   echo "No arguments Provided!";
+   return false;
+   }
+   
+//$name = strip_tags(htmlspecialchars($_POST['name']));
+$email_address = strip_tags(htmlspecialchars($_POST['email']));
+$phone = strip_tags(htmlspecialchars($_POST['phone']));
+$message = strip_tags(htmlspecialchars($_POST['message']));
+   
+// Create the email and send the message
+$to = 'michael.kassabov@gmail.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
+$email_subject = "Website Contact Form:  $first";
+$email_body = "Name: $first\n\nEmail: $email_address\n\nPhone: $phone\n\nMessage:\n$message";
+$headers = "From: noreply@stateridge.us\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
+$headers .= "Reply-To: $email_address";   
+mail($to,$email_subject,$email_body,$headers);
+return true;         
+?>
